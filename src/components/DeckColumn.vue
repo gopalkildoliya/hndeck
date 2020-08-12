e96d46cb4f60cda7ffdf4325fe69edf523c39991
Gopal <gopalkildoliya@gmail.com>
2020-08-12 15:18:07 +0530

    PH Launch added

--- src/components/DeckColumn.vue
+++ src/components/DeckColumn.vue
@@ -4,6 +4,9 @@
             <h2>{{name}}</h2>
         </div>
         <div class="column__list">
+            <div v-if="type=='topstories'" class="border p-3 text-center">
+                <a href="https://www.producthunt.com/posts/hackernews-deck?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-hackernews-deck" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=232165&theme=light" alt="HackerNews Deck - TwitterDeck for Hacker News. Realtime updates. | Product Hunt Embed" style="width: 250px; height: 54px;" width="250px" height="54px" /></a>
+            </div>
             <HnItem v-for="item in items.slice(0, 30)" :key="item['.value']" :itemid="item['.value']" class="border p-3"></HnItem>
         </div>
     </div>
